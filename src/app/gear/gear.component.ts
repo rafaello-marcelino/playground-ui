b9593c1bcab2f6c56d832ed61b44222a797da386
Rafaello Marcelino <rafaello.marcelino@gmail.com>
2023-11-09 22:46:17 -0800

    gear images set to google drive

--- src/app/gear/gear.component.ts
+++ src/app/gear/gear.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./gear.component.css']
 })
 export class GearComponent {
+  // img01: string = "./assets/website/Gear/gear.jpg";
+  // img02: string = "./assets/website/Gear/90D.jpg";
+  // img03: string = "./assets/website/Gear/Lenses.jpg";
+  // img04: string = "./assets/website/Gear/Flash.jpg";
+  // img05: string = "./assets/website/Gear/Tripod.jpg";
 
+  img01: string = "https://drive.google.com/uc?export=view&id=1TtCxoB2dKbyLgcR2PYv66zzl2oCcrP9W";
+  img02: string = "https://drive.google.com/uc?export=view&id=1BlB7baTgTZaql_NSFPvppzhb7ygQwMQd";
+  img03: string = "https://drive.google.com/uc?export=view&id=16ptE5PQkosIDHNS2wuqvCBKLHmx89QmT";
+  img04: string = "https://drive.google.com/uc?export=view&id=1ClfLR5wf3XuX8O5bw4QhlKX063OhGM2D";
+  img05: string = "https://drive.google.com/uc?export=view&id=1XjJJwvpnTndOwcOJZZbJ5RybYNLmhTn4";
 }
